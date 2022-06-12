1d468580c84d5570dbe3b61ac9eeb22c0e200787
atalo pereira <atalopereira@hotmail.com>
2022-06-11 21:04:52 -0300

    ✨ Save email in fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -8,4 +12,23 @@ export default NextAuth({
       authorization: { params: { scope: 'read:user' } }
     }),
   ],
+  secret: process.env.JWT_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile}) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }      
+    },
+  }
 })
